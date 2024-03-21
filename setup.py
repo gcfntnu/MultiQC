e84c2615bc9deef7bf63859201fa6c69f03fa2e9
Geir A <geiramh@stud.ntnu.no>
2024-03-21 14:10:14 +0100

    missing comma

--- setup.py
+++ setup.py
@@ -214,7 +214,7 @@ setup(
             'qiime2 = multiqc.modules.qiime2:MultiqcModule',
             'fastqc_rnaseq = multiqc.modules.fastqc_rnaseq:MultiqcModule',
             'fastv = multiqc.modules.fastv:MultiqcModule',
-            'unitas = multiqc.modules.unitas:MultiqcModule'
+            'unitas = multiqc.modules.unitas:MultiqcModule',
             'parse = multiqc.modules.parse:MultiqcModule'
         ],
         "multiqc.templates.v1": [
